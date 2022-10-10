c628efecf2f58d9da6d25750b3783f33332792ca
ZerADSC <115502641+ZerADSC@users.noreply.github.com>
2022-10-10 14:26:59 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbwDRLbg7gL2rXbarA4_Ru01I1Ut_ykYPe_ElsErhWOil7p_ZrUpXdc_OM32ltMBotUjIA/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbyt6YQvstrgDhlVov40wbOdvwtoFpfWNhXptfuumGG05h6FTMzat60MLUTxL90mIdXK/exec";
